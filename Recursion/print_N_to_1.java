fa1b1fbddc116f2ec188474942037fd05594a037
Prakhar Singh <161489891+codingoclock@users.noreply.github.com>
2025-05-26 00:09:57 +0530

    boom

---
+++ Recursion/print_N_to_1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Main {
+    public static void printNto1(int n)
+    {
+        if(n == 0)
+        {
+            return;
+        }
+        System.out.println(n);
+        
+        printNto1(n-1);
+    }
+    public static void main(String[] args) {
+        printNto1(5);
+    }
+}
